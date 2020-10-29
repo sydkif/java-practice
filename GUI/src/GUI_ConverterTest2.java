4e86e9907698fc5f8e07ae1f42cc8e3b0a7a3457
Syed Akif <57567504+sydkif@users.noreply.github.com>
2020-10-29 13:30:50 +0800

    Update GUI_ConverterTest2.java

--- GUI/src/GUI_ConverterTest2.java
+++ GUI/src/GUI_ConverterTest2.java
@@ -23,19 +23,29 @@ import javax.swing.plaf.basic.BasicComboBoxUI;
 
 public class GUI_ConverterTest2 implements ActionListener {
 
+    // List of declarations
     private static JFrame frame;
     private static JPanel panel;
     private static JMenu menu;
     private static JMenuBar menuBar;
-    private static JMenuItem iL, iW, iT;
-    private static JTextField tf1, tf2;
-    private static JComboBox<String> cb1, cb2;
+    private static JMenuItem iL, iW, iT; // Length, Weight, Temperature
+    private static JTextField tf1, tf2; // textField1, textField2
+    private static JComboBox<String> cb1, cb2; // comboBox1, comboBox2
     private static JLabel label1, label2;
-    private static JButton invBtn;
+    private static JButton invBtn; // invisibleButton
 
     static Color C51 = new Color(51, 51, 51); // Easy way to change color on multiple object
     static Color C31 = new Color(31, 31, 31); // No need to change 1 by 1 in the code
+    // Also used to shorten the code for setColor
 
+    // The MAIN method
+    public static void main(String[] args) {
+
+        new GUI_ConverterTest2();
+
+    }
+
+    // GUI Codes
     GUI_ConverterTest2() {
 
         frame = new JFrame(); // Create frame
@@ -47,7 +57,7 @@ public class GUI_ConverterTest2 implements ActionListener {
         frame.setSize(336, 336); // Default window size
         frame.setTitle("Converter"); // Set window title
         frame.setResizable(false); // Lock window size
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Stop proccess once closed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Stop process once closed
         frame.setJMenuBar(menuBar); // Include the menubar to the frame
         frame.add(panel); // Add panel to the frame
 
@@ -90,41 +100,43 @@ public class GUI_ConverterTest2 implements ActionListener {
         panel.setLayout(null); // To set manually the position of each object
 
         tf1 = new JTextField("Input value"); // Create a text field for input
-        tf1.setBounds(12, 20, 296, 48); // Set the object position
+        tf1.setBounds(12, 20, 296, 48); // Set the object position (x-axis, y-axis, width, height)
         tf1.setBackground(C31); // Set BACKGROUND color
         tf1.setForeground(Color.WHITE); // Set FONT color
         tf1.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 35)); // Set FONT type and size
         tf1.setBorder(BorderFactory.createEmptyBorder()); // To hide white border line
-        // tf1.setVisible(false); // To hide or unhide
-        tf1.setFocusable(false); // Lock it from being edited (Only initialy, later will be unlocked)
+        // tf1.setVisible(false); // To hide or show
+        tf1.setFocusable(false); // Lock it from being edited (Only initially, later will be unlocked)
         panel.add(tf1); // Add into the panel
 
         tf2 = new JTextField("Output value"); // Create a text field for input
-        tf2.setBounds(12, 140, 296, 48); // Set the object position
+        tf2.setBounds(12, 140, 296, 48); // Set the object position (x-axis, y-axis, width, height)
         tf2.setBackground(C31); // Set BACKGROUND color
         tf2.setForeground(Color.WHITE); // Set FONT color
         tf2.setFont(new Font("Segoe UI Light", Font.PLAIN, 35)); // Set FONT type and size
         tf2.setBorder(BorderFactory.createEmptyBorder()); // To hide white border line
-        // tf2.setVisible(false); // To hide or unhide
+        // tf2.setVisible(false); // To hide or show
         tf2.setFocusable(false); // Lock it from being edited
         panel.add(tf2); // Add into the panel
 
-        label1 = new JLabel(""); // Create a label specifically for symbol
-        label1.setBounds(12, 89, 20, 24); // Set the object position
+        label1 = new JLabel(""); // Create a label, in this program, a symbol (can be labeled anything)
+        label1.setBounds(12, 89, 20, 24); // Set the object position (x-axis, y-axis, width, height)
         label1.setForeground(Color.WHITE); // Set FONT color
         label1.setFont(new Font("Segoe MDL2 Assets", Font.PLAIN, 12)); // Set FONT type and size
+        // Segoe MDL2 Assets is a special Windows10 for symbols
         panel.add(label1); // Add the label into panel
-        label1.setVisible(false); // To hide or unhide
+        label1.setVisible(false); // To hide or show
 
-        label2 = new JLabel(""); // Create a label specifically for symbol
-        label2.setBounds(12, 209, 20, 24); // Set the object position
+        label2 = new JLabel(""); // Create a label, in this program, a symbol (can be labeled anything)
+        label2.setBounds(12, 209, 20, 24); // Set the object position (x-axis, y-axis, width, height)
         label2.setForeground(Color.WHITE); // Set FONT color
         label2.setFont(new Font("Segoe MDL2 Assets", Font.PLAIN, 12)); // Set FONT type and size
+        // Segoe MDL2 Assets is a special Windows10 for symbols
         panel.add(label2); // Add the label into panel
-        label2.setVisible(false); // To hide or unhide
+        label2.setVisible(false); // To hide or show
 
         cb1 = new JComboBox<String>(); // Create a combo box
-        cb1.setBounds(28, 88, 120, 24); // Set the object position
+        cb1.setBounds(28, 88, 120, 24); // Set the object position (x-axis, y-axis, width, height)
         cb1.addActionListener(this); // To implement action on click
         cb1.setBackground(C31); // Set BACKGROUND color
         cb1.setForeground(Color.WHITE); // Set FONT color
@@ -135,7 +147,7 @@ public class GUI_ConverterTest2 implements ActionListener {
         cb1.setVisible(false); // Make it invisible initially
 
         cb2 = new JComboBox<String>(); // Create a combo box
-        cb2.setBounds(28, 208, 120, 24); // Set the object position
+        cb2.setBounds(28, 208, 120, 24); // Set the object position (x-axis, y-axis, width, height)
         cb2.addActionListener(this); // To implement action on click
         cb2.setBackground(C31); // Set BACKGROUND color
         cb2.setForeground(Color.WHITE); // Set FONT color
@@ -158,10 +170,10 @@ public class GUI_ConverterTest2 implements ActionListener {
     // This code will run when an action is triggered
     public void actionPerformed(ActionEvent e) {
 
-        cb1.setVisible(true); // Unhide the comboBox 1
-        cb2.setVisible(true); // Unhide the comboBox 2
-        label1.setVisible(true); // Unhide the label 1
-        label2.setVisible(true); // Unhide the label 2
+        cb1.setVisible(true); // Show the comboBox 1
+        cb2.setVisible(true); // Show the comboBox 2
+        label1.setVisible(true); // Show the label 1
+        label2.setVisible(true); // Show the label 2
         tf1.setFocusable(true); // Unlock textField 1
 
         // Run this code when LENGTH is selected
@@ -171,9 +183,10 @@ public class GUI_ConverterTest2 implements ActionListener {
 
             // List of item inside comboBox
             String[] lengthList = { "Centimeters", "Meters", "Kilometers", "Inches", "Feet", "Miles" };
-            // Apply same list for both comboBox
+            // Create model for the list
             DefaultComboBoxModel<String> model1 = new DefaultComboBoxModel<>(lengthList);
             DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>(lengthList);
+            // Apply model to comboBox
             cb1.setModel(model1);
             cb2.setModel(model2);
 
@@ -188,9 +201,10 @@ public class GUI_ConverterTest2 implements ActionListener {
 
             // List of item inside comboBox
             String[] weightList = { "Milligrams", "Grams", "Kilograms", "Metric tonnes", "Pounds" };
-            // Apply same list for both comboBox
+            // Create model for the list
             DefaultComboBoxModel<String> model1 = new DefaultComboBoxModel<>(weightList);
             DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>(weightList);
+            // Apply model to comboBox
             cb1.setModel(model1);
             cb2.setModel(model2);
 
@@ -204,10 +218,11 @@ public class GUI_ConverterTest2 implements ActionListener {
             reset(); // Reset both value to 0
 
             // List of item inside comboBox
-            String[] tempList = { "Celcius", "Farenheit", "Kelvin" };
-            // Apply same list for both comboBox
+            String[] tempList = { "Celsius", "Fahrenheit", "Kelvin" };
+            // Create model for the list
             DefaultComboBoxModel<String> model1 = new DefaultComboBoxModel<>(tempList);
             DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>(tempList);
+            // Apply model to comboBox
             cb1.setModel(model1);
             cb2.setModel(model2);
 
@@ -224,58 +239,58 @@ public class GUI_ConverterTest2 implements ActionListener {
             LengthConverter from = new LengthConverter(s1);
             LengthConverter to = new LengthConverter(s2);
 
-            double val = Double.parseDouble(tf1.getText()); // Get input from textfield1 as DOUBLE
+            double val = Double.parseDouble(tf1.getText()); // Get input from textField1 as DOUBLE
             double meters = from.toMeters(val);
             double converted = to.fromMeters(meters);
 
             converted = round(converted, 5); // Rounded the number to 5 digits
-            tf2.setText(String.valueOf(converted)); // Change textfield2 to output value
+            tf2.setText(String.valueOf(converted)); // Change textField2 to output value
 
         } else if (s == "≡   Weight") { // If WEIGHT is selected run these codes
 
             WeightConverter from = new WeightConverter(s1);
             WeightConverter to = new WeightConverter(s2);
 
-            double val = Double.parseDouble(tf1.getText()); // Get input from textfield1 as DOUBLE
+            double val = Double.parseDouble(tf1.getText()); // Get input from textField1 as DOUBLE
             double kilograms = from.toKilograms(val);
             double converted = to.fromKilograms(kilograms);
 
             converted = round(converted, 5); // Rounded the number to 5 digits
-            tf2.setText(String.valueOf(converted)); // Change textfield2 to output value
+            tf2.setText(String.valueOf(converted)); // Change textField2 to output value
 
         } else if (s == "≡   Temperature") { // If TEMPERATURE is selected run these codes
 
             double C, F, K, converted = 0;
 
-            if (s1.equals("Celcius")) { // Celcius to others
+            if (s1.equals("Celsius")) { // Celsius to others
 
-                C = Double.parseDouble(tf1.getText()); // Get input from textfield1 as DOUBLE
+                C = Double.parseDouble(tf1.getText()); // Get input from textField1 as DOUBLE
 
-                if (s2.equals("Farenheit")) // C to F
+                if (s2.equals("Fahrenheit")) // C to F
                     converted = (C * 9 / 5) + 32;
                 else if (s2.equals("Kelvin")) // C to K
                     converted = C + 273.15;
                 else // C to C
                     converted = C;
 
-            } else if (s1.equals("Farenheit")) { // Farenheit to others
+            } else if (s1.equals("Fahrenheit")) { // Fahrenheit to others
 
                 F = Double.parseDouble(tf1.getText());
 
-                if (s2.equals("Celcius")) // F to C
+                if (s2.equals("Celsius")) // F to C
                     converted = (F - 32) * 5 / 9;
                 else if (s2.equals("Kelvin")) // F to K
                     converted = (F - 32) * 5 / 9 + 273.15;
                 else // F to F
                     converted = F;
 
-            } else if (s1.equals("Kelvin")) { // Kelvin to othersv
+            } else if (s1.equals("Kelvin")) { // Kelvin to others
 
                 K = Double.parseDouble(tf1.getText());
 
-                if (s2.equals("Farenheit")) // K to F
+                if (s2.equals("Fahrenheit")) // K to F
                     converted = (K - 273.15) * 9 / 5 + 32;
-                else if (s2.equals("Celcius")) // K to C
+                else if (s2.equals("Celsius")) // K to C
                     converted = K - 273.15;
                 else // K to K
                     converted = K;
@@ -283,14 +298,14 @@ public class GUI_ConverterTest2 implements ActionListener {
             }
 
             converted = round(converted, 5); // Rounded the number to 5 digits
-            tf2.setText(String.valueOf(converted)); // Change textfield2 to output value
+            tf2.setText(String.valueOf(converted)); // Change textField2 to output value
 
         }
     }
 
     // reset function
     static void reset() {
-        tf1.setText("0"); // Make both textfield
+        tf1.setText("0"); // Make both textField
         tf2.setText("0"); // to 0
     }
 
@@ -305,31 +320,6 @@ public class GUI_ConverterTest2 implements ActionListener {
         return bd.doubleValue();
     }
 
-    // The MAIN method
-    public static void main(String[] args) {
-
-        new GUI_ConverterTest2();
-
-    }
-
-}
-
-// Copy pasted from internet.
-// To create custom flexible desing for combobox
-// Make it all black to merge with background
-// to achieve clean looks.
-class ColorArrowUI extends BasicComboBoxUI {
-
-    static Color BLANK = new Color(31, 31, 31);
-
-    public static ComboBoxUI createUI(JComponent c) {
-        return new ColorArrowUI();
-    }
-
-    @Override
-    protected JButton createArrowButton() {
-        return new BasicArrowButton(BasicArrowButton.SOUTH, BLANK, BLANK, BLANK, BLANK);
-    }
 }
 
 // Conversion class for LENGTH
@@ -403,5 +393,22 @@ class WeightConverter {
 
     public double fromKilograms(double measurement) {
         return (measurement / factor);
+    }
+}
+
+// Copy pasted from internet.
+// To create custom flexible design for comboBox
+// Make it all black to merge with background.
+class ColorArrowUI extends BasicComboBoxUI {
+
+    static Color BLANK = new Color(31, 31, 31);
+
+    public static ComboBoxUI createUI(JComponent c) {
+        return new ColorArrowUI();
+    }
+
+    @Override
+    protected JButton createArrowButton() {
+        return new BasicArrowButton(BasicArrowButton.SOUTH, BLANK, BLANK, BLANK, BLANK);
     }
 }
